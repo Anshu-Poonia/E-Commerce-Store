e441db9894f94f88d7e7dcd1a146e6c203b5bd99
Anshu-Poonia <anshupoonia902005@gmail.com>
2026-08-07 22:47:35 +0530

    Initializing the product, cart and coupen routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  refreshToken,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -7,5 +12,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/refresh-token", refreshToken);
+// router.get("/profile", getProfile);
 
 export default router;
