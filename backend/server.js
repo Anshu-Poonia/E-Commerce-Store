00d8488f2037037728fc4c8bda712ccd6b5badeb
Anshu-Poonia <anshupoonia902005@gmail.com>
2026-08-08 12:06:16 +0530

    Implementing the payment and analytics route

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.route.js";  //Import the auth routes
-import productRoutes from "./routes/product.route.js";  //Import the product routes
-import cartRoutes from "./routes/cart.route.js";  //Import the cart routes
-import coupenRoutes from "./routes/coupen.route.js";  //Import the coupen routes
+import authRoutes from "./routes/auth.route.js"; //Import the auth routes
+import productRoutes from "./routes/product.route.js"; //Import the product routes
+import cartRoutes from "./routes/cart.route.js"; //Import the cart routes
+import coupenRoutes from "./routes/coupen.route.js"; //Import the coupen routes
+import paymentRoutes from "./routes/payment.route.js"; //Import the payment routes
+import analyticsRoutes from "./routes/analytics.route.js"; //Import the analytics routes
 
 import { connectDB } from "./lib/db.js";
 
-dotenv.config();  // Allow to read the content of .env file
+dotenv.config(); // Allow to read the content of .env file
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());  // allow you to parse the body of the request as JSON
-app.use(cookieParser());  // Allow to parse cookies
+app.use(express.json()); // allow you to parse the body of the request as JSON
+app.use(cookieParser()); // Allow to parse cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupens", coupenRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    connectDB();
+  console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
